920ab931009f1783d365694647553ccae51e72e7
dsgrammer <dsgrammer85@gmail.com>
2023-04-28 00:19:34 +0900

    data preprocessing, round 1 finished, tinkering with model

--- 01 Scripts/Titanic-ML-Script.R
+++ 01 Scripts/Titanic-ML-Script.R
@@ -1,21 +1,66 @@
 setwd("C:/Users/dsgra/OneDrive/Documents/GitHub/Titanic-Machine-Learning-from-Disaster")
 library(tidyverse)
+train = read.csv('C:/Users/dsgra/OneDrive/Documents/GitHub/Titanic-Machine-Learning-from-Disaster/00 Data/train.csv',
+                 na.strings = c(""), stringsAsFactors=TRUE)
+test = read.csv('C:/Users/dsgra/OneDrive/Documents/GitHub/Titanic-Machine-Learning-from-Disaster/00 Data/test.csv',
+                na.strings = c(""), stringsAsFactors=TRUE)
 
-train <- readr::read_csv('C:/Users/dsgra/OneDrive/Documents/GitHub/Titanic-Machine-Learning-from-Disaster/00 Data/train.csv')
-test <- readr::read_csv('C:/Users/dsgra/OneDrive/Documents/GitHub/Titanic-Machine-Learning-from-Disaster/00 Data/test.csv')
+#Data Preprocessing -------------------------------------------------------------------
+#Convert columns into factors if they are factors
+str(train)
+train$Survived <- factor(train$Survived)
+train$Pclass <- factor(train$Pclass)
 
+#Before dropping columns and dealing with missing values take backup of original data
+train_backup <- train
+train[!complete.cases(train),]
+
+#Cabin variable is nearly all NA, dropping as it is impossible to estimate cabin numbers
+train <- subset(train,select = -c(Cabin))
+
+#Age has 177 NAs, how to fill... Could replace with median,but let's wait and see.
+table(train$Age, useNA = 'always')
+summary(train$Age)
+# table(train$PassengerId, useNA = 'always')
+# table(train$Pclass, useNA = 'always')
+# table(train$Name, useNA = 'always')
+# table(train$Sex, useNA = 'always')
+# table(train$SibSp, useNA = 'always')
+# table(train$Parch, useNA = 'always')
+# table(train$Ticket, useNA = 'always')
+# table(train$Fare, useNA = 'always')
+# Two passengers did not board the titanic, dropped
+table(train$Embarked, useNA = 'always')
+train <- train[!is.na(train$Embarked),]
+
+str(test)
+str(train)
+test$Pclass <- factor(test$Pclass)
+test_backup <- test
+table(test$Cabin, useNA = 'always')
+test <- subset(test,select = -c(Cabin))
+#What to do about age...
+table(train$Age, useNA = 'always')
+summary(train$Age)
+# Two passengers did not board the titanic, dropped
+table(test$Embarked, useNA = 'always')
+# test <- test %>%
+#   add_column(empty_column = NA)
+#----------------------------------------------------------------------------------
 # Fitting XGBoost to the Training set
-# install.packages('xgboost')
 library(xgboost)
-classifier = xgboost(data = as.matrix(train[2]), label = train$Survived, nrounds = 10)
+data <- as.matrix(train[-2])
+label <- as.numeric(train$Survived)
+mode(data) <- 'double'
+classifier = xgboost(data = data, label = label, nrounds = 10, objective = "binary:logistic")
 
 # Predicting the Test set results
-y_pred = predict(classifier, newdata = as.matrix(test[1,11]))
+newdata = as.matrix(test[-11])
+mode(newdata) <- 'double'
+y_pred = predict(classifier, newdata = newdata)
 y_pred = (y_pred >= 0.5)
 
-# Making the Confusion Matrix
-cm = table(test_set[, 11], y_pred)
-
+y_pred
 # Applying k-Fold Cross Validation
 # install.packages('caret')
 library(caret)
@@ -23,7 +68,7 @@ folds = createFolds(dataset$Exited, k = 10)
 cv = lapply(folds, function(x) {
   training_fold = dataset[-x, ]
   test_fold = dataset[x, ]
-  classifier = xgboost(data = as.matrix(training_fold[-11]), label = training_fold$Exited, nrounds = 10)
+  classifier = xgboost(data = data, label = label, nrounds = 10)
   y_pred = predict(classifier, newdata = as.matrix(test_fold[-11]))
   y_pred = (y_pred >= 0.5)
   cm = table(test_fold[, 11], y_pred)
